54c51e88c40503448b4b85638270b8f8915aefe1
nbogie <neillbogie@googlemail.com>
2023-07-14 22:20:48 +0100

    docs

--- src/core/fancyFilters.ts
+++ src/core/fancyFilters.ts
@@ -1,18 +1,23 @@
-//Alternative, and perhaps unnecessarily complex way to implement filters, making the main react component cleaner.
+//Alternative (and perhaps unnecessarily complex) way to implement filters, making the main react component cleaner.
 //Note: none of these know anything about React.
 import { BabyName, SexFilter } from "./babyName";
-export type PredFn<T> = (item: T) => boolean;
+export type PredicateFunction<T> = (item: T) => boolean;
 
-export function bySearch(searchTerm: string): PredFn<BabyName> {
+/** Create a predicate function which passes if a given baby name matches the given searchTerm */
+export function bySearch(searchTerm: string): PredicateFunction<BabyName> {
     return (nameData: BabyName) =>
         nameData.name.toLowerCase().includes(searchTerm.toLowerCase());
 }
 
-export function bySex(sexFilter: SexFilter): PredFn<BabyName> {
+/** Create a predicate function which passes if a given baby name matches with the given sex filter */
+export function bySex(sexFilter: SexFilter): PredicateFunction<BabyName> {
     return (nameData: BabyName) =>
         sexFilter === "a" || sexFilter === nameData.sex;
 }
 
-export function byNotInList(favourites: BabyName[]): PredFn<BabyName> {
+/** Create a predicate function which passes if a given baby name is not in the given list of favourite names. */
+export function byNotInList(
+    favourites: BabyName[]
+): PredicateFunction<BabyName> {
     return (name: BabyName) => !favourites.find((f) => f.id === name.id);
 }
